0ddbe0cf43cbcdbbe3ba5f65dd258a9e059e91cc
Dina <dinaobnaglela@yandex.ru>
2024-12-14 13:25:48 +0300

    Добавьте файлы проекта.

---
+++ StaticConstructor/Book.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System;
+
+namespace StaticConstructor
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public Book(string title)
+        {
+            Console.WriteLine("Вызван обычный конструктор");
+            Title = title;
+        }
+        static Book()
+        {
+            Console.WriteLine("Вызван статический конструктор");
+        }
+    }
+}
---
+++ StaticConstructor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StaticConstructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b1 = new Book("Война и мир");
+            Book b2 = new Book("Преступление и наказание");
+
+            Console.ReadKey();
+        }
+    }
+}
